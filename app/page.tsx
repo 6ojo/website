5d539d0f351e9dca5787be986e1c947af0a0c36b
gojo <github@gojo.mozmail.com>
2023-06-28 23:58:48 -0700

    getting rid of projects?
    unneeded but idk if this will workk

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-	{ name: "projects", href: "/projects" },
 	{ name: "contact", href: "/contact" },
 ];
 
