32ae504ff32a24dec612bad9470e367a03a5f02b
gojo <github@gojo.mozmail.com>
2023-06-28 18:12:49 -0700

    playing around

--- app/page.tsx
+++ app/page.tsx
@@ -29,20 +29,19 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				gojo
+				gojo.one
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					hey, im gojo. i do things sometimes on my {" "}
 					<Link
 						target="_blank"
-						href="https://upstash.com"
+						href="https:/github.com/6ojo"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Upstash
+						github
 					</Link>
 
 					<br />
