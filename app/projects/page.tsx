f0c49f240a3cac0f5d270243486bad7c7797245c
gojo <github@gojo.mozmail.com>
2023-07-01 02:06:02 -0700

    getting rid of unused project?

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -24,7 +24,6 @@ export default async function ProjectsPage() {
 		(project) => project.slug === "newsite",
 	)!;
 	const top2 = allProjects.find((project) => project.slug === "oldsite")!;
-	const top3 = allProjects.find((project) => project.slug === "envshare")!;
 	const sorted = allProjects
 		.filter((p) => p.published)
 		.filter(
