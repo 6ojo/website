6df8a0afc84efdb9f6e0c7ae8d99ba554bebe583
6ojo <wk92g3r4r@mozmail.com>
2023-07-29 19:21:42 -0700

    JKSDGDJKHG

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,8 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent {
-
-  title = 'gojo- home';
-
-  constructor(private titleService: Title) { }
-
-  setTitle(newTitle: string) {
-    this.titleService.setTitle(newTitle);
-  }
-
-}
+import {document} from "document";
+document.title = 'gojo- home';
 
 
 const navigation = [
