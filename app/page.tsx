6421fba2f34e6b32bed62c25fcb24cd9cc2d1aa0
6ojo <wk92g3r4r@mozmail.com>
2023-07-29 19:18:42 -0700

    differnt

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import {document} from 'document';
-document.title = 'gojo- home';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent {
+
+  title = 'gojo- home';
+
+  constructor(private titleService: Title) { }
+
+  setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+
+}
 
 
 const navigation = [
