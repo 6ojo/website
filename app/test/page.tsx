1a09f33a9499c17dadd484ac31935c827954b91e
6ojo <wk92g3r4r@mozmail.com>
2023-09-19 18:06:28 -0700

    hmmmmm

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -14,15 +14,6 @@ export default function Home() {
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex items-center justify-center gap-4">
-					{navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
-					))}
 				</ul>
 			</nav>
 			<Particles
