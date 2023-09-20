90deb2f376bac409f5f99a263f0a651dbf7e9af2
6ojo <wk92g3r4r@mozmail.com>
2023-09-19 18:22:45 -0700

    i could do that!

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 				quantity={500}
 			/>
 
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
 					example of what id say about myself {" "}
@@ -43,7 +43,7 @@ export default function Home() {
 						go home
 					</Link>
 				</h2>
-			</div>
+				</div>
 		</div>
 	);
 }
