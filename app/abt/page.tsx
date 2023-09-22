b6cf3d1a77c572a7feb75b252358d76847968e3d
6ojo <wk92g3r4r@mozmail.com>
2023-09-21 18:30:11 -0700

    responsive sites suck
    just have a 1920*1080 phone 4head

--- app/abt/page.tsx
+++ app/abt/page.tsx
@@ -1,50 +1,64 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "app/components/particles";
-// import { Navigation } from "../components/nav";
-const imageUrl = "https://media.discordapp.net/attachments/1010995350624014407/1154260384555999262/gojo.png?width=670&height=670"
+import { useEffect, useState } from "react";
 
+const imageUrl = "https://media.discordapp.net/attachments/1010995350624014407/1154260384555999262/gojo.png?width=670&height=670";
 
 export default function Home() {
-	return (
-		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-			<Particles
-				className="absolute inset-0 -z-10"
-				quantity={500}
-			/>
-			<img 
-				src={imageUrl}
-				width="335"
-				height="335"
-			/>
-			<div className="my-16 text-center">
-				<h1 className="z-10 text-4xl text-zinc-500 text-transparent cursor-default bg-white whitespace-nowrap bg-clip-text font-display"
-					style={{color: "#ffffff"}}
-				>
-					im gojo and i want to be your friend
-				</h1>
+  const [isMobile, setIsMobile] = useState(false);
 
-				<h2 className="text-2xl text-zinc-500"
-				style={{color: "#ffffff"}}
-				>
-					thanks for visiting my site <br /> i like cats, records, and being silly on the interweb
-				
-				</h2>
-				<h3 className="text-xl text-zinc-500">	
-				<br />
-					{" "}
-					<Link
-						target="_blank"
-						href="https://www.gojo.one"
-						className="underline duration-500 hover:text-zinc-300"
-					>				
-						go home
-					</Link>
-					</h3>
-				</div>
-		</div>
-	);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black ${
+        isMobile ? "p-4" : ""
+      }`}
+    >
+      <Particles className="absolute inset-0 -z-10" quantity={500} />
+      <img src={imageUrl} width="335" height="335" />
+      <div className="my-16 text-center">
+        <h1
+          className={`z-10 text-4xl text-zinc-500 text-transparent cursor-default bg-white whitespace-nowrap bg-clip-text font-display ${
+            isMobile ? "text-2xl" : ""
+          }`}
+          style={{ color: "#ffffff" }}
+        >
+          im gojo and i want to be your friend
+        </h1>
+
+        <h2
+          className={`text-2xl text-zinc-500 ${
+            isMobile ? "text-lg" : ""
+          }`}
+          style={{ color: "#ffffff" }}
+        >
+          thanks for visiting my site <br /> i like cats, records, and being
+          silly on the interweb
+        </h2>
+        <h3 className="text-xl text-zinc-500">
+          <br />
+          <Link
+            target="_blank"
+            href="https://www.gojo.one"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            go home
+          </Link>
+        </h3>
+      </div>
+    </div>
+  );
 }
-{/* <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				about
-			</h1> */}
