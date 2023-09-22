168a30f3de47993a49d30af24f4fac30b9f74651
6ojo <wk92g3r4r@mozmail.com>
2023-09-21 19:15:51 -0700

    idk what im odoing

--- app/abt/page.tsx
+++ app/abt/page.tsx
@@ -16,11 +16,11 @@ export default function Home() {
       <div className="content font-display my-16 text-center ">
         <h1 className="title"> im gojo and i want to be your friend</h1>
 
-        <h2 className="subtitle my-16 text-center animate-fade-in ">
+        <h2 className="subtitle my-16 text-center ">
           thanks for visiting my site. <br /> i like cats, records, and being silly on the interweb.
         </h2>
 
-        <h3 className="link my-16 text-center  animate-fade-in">
+        <h3 className="link my-16 text-center animate-fade-in">
           <Link target="_self" href="https://www.gojo.one" className="underline">
             go home
           </Link>
