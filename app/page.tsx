4337762781b21284525ac6266d921c41d82303fa
6ojo <wk92g3r4r@mozmail.com>
2024-11-16 21:44:56 -0700

    make it 24hr time

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 		script.onload = () => {
 			if (window.time_is_widget) {
 				window.time_is_widget.init({
-					MT_z12b: { time_format: "12hours:minutes:secondsA" },
+					MT_z12b: { time_format: "hours:minutes" },
 				});
 			}
 		};
